2b79d4d06983982f1af64c19afd496ee3cc9983d
Jeremyispapo <jeremyispapo@gmail.com>
2020-12-25 23:08:33 -0500

    Agrege funcionalidad a mi boton more

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const menu = document.querySelector('header');
 const burgerButtom = document.querySelector('.icon');
+const more = document.querySelector('.more');
+const ventana = document.querySelector('aside');
 const img = document.querySelectorAll('img');
 
     burgerButtom.addEventListener('click', mostrar);
-    // img.addEventListener('click', aclarar)
+    more.addEventListener('click', aside)
 
     function mostrar() {
         menu.classList.toggle('active')
 }
+
+    function aside() {
+        ventana.classList.toggle('active');
+    }
 
 for (i = 0; i < img.length; i++) {
     img[i].addEventListener('click', aclarar);
