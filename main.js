e6fbf42a84a150aab033f02fa5d3f9a8f813bca2
Jeremyispapo <jeremyispapo@gmail.com>
2020-12-26 13:13:37 -0500

    Agrege los bugs de navegacion y lo adapte a computadoras

--- main.js
+++ main.js
@@ -3,23 +3,46 @@ const burgerButtom = document.querySelector('.icon');
 const more = document.querySelector('.more');
 const ventana = document.querySelector('aside');
 const img = document.querySelectorAll('img');
+const btnInicio = document.querySelector('#btn-inicio');
+const btnMenu = document.querySelector('#btn-menu');
+const btnGaleria = document.querySelector('#btn-galeria');
+const icons = window.matchMedia('screen and (max-width: 850px)')
 
-    burgerButtom.addEventListener('click', mostrar);
-    more.addEventListener('click', aside)
+    icons.addEventListener('change', off)
+
+
+    function off(event) {
+        if (event.matches) {
+            burgerButtom.addEventListener('click', mostrar);
+            btnInicio.addEventListener('click', mostrar);
+            btnMenu.addEventListener('click', mostrar);
+            btnGaleria.addEventListener('click', mostrar);
+            more.addEventListener('click', aside);
+        } else {
+            alert('adios');
+            burgerButtom.removeEventListener('click', mostrar)
+            btnInicio.removeEventListener('click', mostrar);
+            btnMenu.removeEventListener('click', mostrar);
+            btnGaleria.removeEventListener('click', mostrar);
+            more.removeEventListener('click', aside);
+        }
+    }
 
     function mostrar() {
-        menu.classList.toggle('active')
-}
+        menu.classList.toggle('active');
+    }
 
     function aside() {
         ventana.classList.toggle('active');
     }
 
-for (i = 0; i < img.length; i++) {
-    img[i].addEventListener('click', aclarar);
+    for (i = 0; i < img.length; i++) {
+        img[i].addEventListener('click', aclarar);
 
-    function aclarar() {
+        function aclarar() {
         this.classList.toggle('active');
+        }
+
     }
 
-}
+    off(icons)
